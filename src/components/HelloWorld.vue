c4b515ed170516ed346f2638e9110421a4914123
elontian <elontian@tencent.com>
2020-06-17 15:12:56 +0800

    增加debug模式，整合cloudbase for vue库

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,85 +1,12 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+    <template>
+      <LoginState v-slot="{ loginState }">
+        <h2>登录云开发</h2>
+        <p>{{ loginState ? "已登录" : "未登录" }}</p>
+      </LoginState>
+    </template>
   </div>
 </template>
 
@@ -88,7 +15,27 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: "Welcome to Elon's Vue e TCB.js App"
+    }
+  },
+  async created () {
+    this.envId = this.$cloudbase.config.env
+
+    try {
+      let ret = await this.$cloudbase
+        .auth({ presistence: 'session' })
+        .anonymousAuthProvider()
+        .signIn()
+
+      console.log('login')
+      console.log(ret)
+    } catch (e) {
+      if (e.message.includes('100007')) {
+        this.isLoginSuccess = false
+      }
+
+      console.error(e)
+      console.log(e.code)
     }
   }
 }
@@ -96,7 +43,8 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 ul {
--- src/main.js
+++ src/main.js
@@ -2,8 +2,16 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import Cloudbase from '@cloudbase/vue-provider'
+import config from '../cloudbaserc'
+// eslint-disable-next-line no-unused-vars
+import babelPolyfill from 'babel-polyfill'
 
 Vue.config.productionTip = false
+
+Vue.use(Cloudbase, {
+  env: config.envId
+})
 
 /* eslint-disable no-new */
 new Vue({
